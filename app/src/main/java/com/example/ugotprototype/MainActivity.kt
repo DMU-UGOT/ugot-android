1e91106d04e0763abc821384d430defbbb41dd8d
songkichan <8345784@naver.com>
2023-05-01 21:26:50 +0900

    로그인 -> 홈 액티비티 이동

--- app/src/main/java/com/example/ugotprototype/MainActivity.kt
+++ app/src/main/java/com/example/ugotprototype/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.ugotprototype
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ugotprototype.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 바인딩 객체 선언
+    private var viewBinding: ActivityMainBinding? = null
+    // null체크 편의성을 위한 바인딩 변수 재선언
+    private val binding get() = viewBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 뷰 객체화
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btGohomeactivity.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish();
+        }
+
+
+
+
     }
 }
