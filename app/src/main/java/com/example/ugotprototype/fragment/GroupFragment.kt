d3d5de3c6c5329f37b6bdd12168e4ce302251f29
songkichan <8345784@naver.com>
2023-05-01 21:26:30 +0900

    기능별 xml, kotlin 파일 생성

---
+++ app/src/main/java/com/example/ugotprototype/fragment/GroupFragment.kt
@@ -0,0 +1,15 @@
+package com.example.ugotprototype.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ugotprototype.R
+
+class GroupFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_group, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/ugotprototype/fragment/StudyFragment.kt
@@ -0,0 +1,15 @@
+package com.example.ugotprototype.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ugotprototype.R
+
+class StudyFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_study, container, false)
+    }
+}
