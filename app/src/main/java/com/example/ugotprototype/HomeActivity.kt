52342155f62b2c27a20d819ff47910d4f75e1e9d
songkichan <8345784@naver.com>
2023-05-02 12:27:08 +0900

    View Binding -> Data Binding 교체

--- app/src/main/java/com/example/ugotprototype/HomeActivity.kt
+++ app/src/main/java/com/example/ugotprototype/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.ugotprototype
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -11,17 +12,15 @@ import com.example.ugotprototype.databinding.ActivityHomeBinding
 
 class HomeActivity :AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var _binding: ActivityHomeBinding
-
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(_binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
-        _binding.bnMenu.setupWithNavController(navController)
+        binding.bnMenu.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/ugotprototype/MainActivity.kt
+++ app/src/main/java/com/example/ugotprototype/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.ugotprototype
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.ugotprototype.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    // 바인딩 객체 선언
-    private var viewBinding: ActivityMainBinding? = null
-    // null체크 편의성을 위한 바인딩 변수 재선언
-    private val binding get() = viewBinding!!
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 뷰 객체화
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.btGohomeactivity.setOnClickListener{
             val intent = Intent(this, HomeActivity::class.java)
