20c72718e6abf53f64b925eb6979eeafdeaf197c
KriverM <star6sky@naver.com>
2023-05-16 15:07:08 +0900

    my profile layout feedback 반영

--- app/src/main/java/com/example/ugotprototype/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ugotprototype/ui/profile/ProfileFragment.kt
@@ -1,14 +1,55 @@
 package com.example.ugotprototype.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.ugotprototype.R
+import com.example.ugotprototype.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+class ProfileFragment : AppCompatActivity() {
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.fragment_profile)
+
+
+// 학교 정보 확인
+        binding.profileSchoolBtn.setOnClickListener {
+            startActivity(Intent(this, SchoolActivity::class.java))
+            finish()
+        }
+
+
+// 사용 기술
+        binding.profileStackBtn.setOnClickListener {
+            startActivity(Intent(this, StackActivity::class.java))
+            finish()
+        }
+
+// 회원 탈퇴
+        binding.profileLogoutBtn.setOnClickListener {
+
+
+        }
+
+// 로그 아웃
+        binding.profileLogoutBtn.setOnClickListener {
+
+
+        }
+
+// 알림 설정
+        binding.profileAlarmOptionBtn.setOnClickListener {
+
+        }
+
+// 채팅 설정
+        binding.profileChatOptionBtn.setOnClickListener {
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/ugotprototype/ui/profile/SchoolActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ugotprototype.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ugotprototype.R
+
+class SchoolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_school)
+
+    }
+}
