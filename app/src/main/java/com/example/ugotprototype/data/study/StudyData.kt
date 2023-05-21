2a2d523efbda934b4059f39a63cd5b368eedac08
KriverM <star6sky@naver.com>
2023-05-21 23:34:50 +0900

    recyclerView 를 위한 item_list 제작

---
+++ app/src/main/java/com/example/ugotprototype/data/study/StudyData.kt
@@ -0,0 +1,9 @@
+package com.example.ugotprototype.data.study
+
+class StudyData (
+    val title : String, // teamName 제목
+    val text : String, // text 글 내용
+    val inquire : Int, // inquire 조회 수
+    val meet : String, // meet 대면 여부
+    val member : String // member 인원 수
+)
--- app/src/main/java/com/example/ugotprototype/ui/study/StudyFragment.kt
+++ app/src/main/java/com/example/ugotprototype/ui/study/StudyFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.ugotprototype.R
+import com.example.ugotprototype.databinding.FragmentStudyBinding
 
 class StudyFragment : Fragment() {
+    private lateinit var binding: FragmentStudyBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_study, container, false)
     }
