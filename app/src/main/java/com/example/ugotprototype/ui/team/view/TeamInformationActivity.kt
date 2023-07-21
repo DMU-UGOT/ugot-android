31f14a5208fd69ed08dd3137bae5229050e460d3
songkichan <8345784@naver.com>
2023-07-21 17:54:27 +0900

    팀빌딩 팀원현황 페이지 이미지, 링크, 아이디 요청

--- app/src/main/java/com/example/ugotprototype/ui/team/view/TeamInformationActivity.kt
+++ app/src/main/java/com/example/ugotprototype/ui/team/view/TeamInformationActivity.kt
@@ -2,14 +2,31 @@ package com.example.ugotprototype.ui.team.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.example.ugotprototype.R
 import com.example.ugotprototype.databinding.ActivityTeamInformationBinding
+import com.example.ugotprototype.di.api.ApiService
+import com.example.ugotprototype.di.api.response.OrgMemberDataResponse
 import com.example.ugotprototype.ui.team.adapter.TeamInformationRecyclerViewAdapter
+import com.example.ugotprototype.ui.team.viewmodel.TeamViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class TeamInformationActivity : AppCompatActivity() {
+    @Inject
+    lateinit var apiService: ApiService
+
     private lateinit var binding: ActivityTeamInformationBinding
     private var rvAdapter: TeamInformationRecyclerViewAdapter = TeamInformationRecyclerViewAdapter()
+    private val teamViewModel: TeamViewModel by viewModels()
+
+    companion object {
+        var githubOrgName = "githubapi-testad"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +37,23 @@ class TeamInformationActivity : AppCompatActivity() {
         }
 
         dataSet()
+
+        teamViewModel.isTeamInforList.observe(this) {
+            rvDataSet(it)
+        }
     }
 
     private fun dataSet() {
-        val nowPersonCnt: Int = intent.getStringExtra("nowPersonCnt")?.toInt() ?: 0
-
         binding.rvTeamInformation.adapter = rvAdapter
-        rvAdapter.setData(nowPersonCnt)
+        lifecycleScope.launch {
+            try {
+                teamViewModel.setTeamInforData(apiService.getOrganizationMembers(githubOrgName))
+            } catch (_: Exception) {
+            }
+        }
+    }
+
+    private fun rvDataSet(teamInforData: List<OrgMemberDataResponse>) {
+        rvAdapter.setData(teamInforData)
     }
 }
