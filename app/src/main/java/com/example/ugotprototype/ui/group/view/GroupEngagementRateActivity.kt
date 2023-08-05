dcbcca289a7051d20543b02b0d6106d112d75663
songkichan <8345784@naver.com>
2023-08-05 11:32:19 +0900

    그룹페이지 참여도 api 요청 데이터 처리

--- app/src/main/java/com/example/ugotprototype/ui/group/view/GroupEngagementRateActivity.kt
+++ app/src/main/java/com/example/ugotprototype/ui/group/view/GroupEngagementRateActivity.kt
@@ -1,25 +1,32 @@
 package com.example.ugotprototype.ui.group.view
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.constraintlayout.widget.Group
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.ugotprototype.R
-import com.example.ugotprototype.data.group.GroupEngagementRateData
-import com.example.ugotprototype.data.group.GroupTopViewData
-import com.example.ugotprototype.databinding.ActivityGroupDetailCalendarBinding
+import com.example.ugotprototype.data.group.GroupEngagementData
 import com.example.ugotprototype.databinding.ActivityGroupEngagementRateBinding
+import com.example.ugotprototype.di.api.ApiService
 import com.example.ugotprototype.ui.group.adapter.GroupEngagementRateRecyclerViewAdapter
-import com.example.ugotprototype.ui.group.adapter.GroupTopViewRecyclerViewAdapter
 import com.example.ugotprototype.ui.group.viewmodel.GroupViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class GroupEngagementRateActivity : AppCompatActivity() {
+    @Inject
+    lateinit var apiService: ApiService
 
     private lateinit var binding: ActivityGroupEngagementRateBinding
     private lateinit var groupEngagementRateRecyclerViewAdapter: GroupEngagementRateRecyclerViewAdapter
-    private var groupEngagementRateItem = ArrayList<GroupEngagementRateData>()
+
+    private var groupEngagementData = ArrayList<GroupEngagementData>()
 
     private val groupViewModel: GroupViewModel by viewModels()
 
@@ -27,52 +34,46 @@ class GroupEngagementRateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_group_engagement_rate)
 
+        fetchMembers()
+
         groupEngagementRateRecyclerViewAdapter = GroupEngagementRateRecyclerViewAdapter()
         binding.rvRating.adapter = groupEngagementRateRecyclerViewAdapter
 
-        testData()
-        groupViewModel.setEngagementRate(groupEngagementRateItem)
-
         groupViewModel.engagementRateData.observe(this) {
-            groupEngagementRateRecyclerViewAdapter.setData(groupEngagementRateItem)
+            groupEngagementRateRecyclerViewAdapter.setData(groupEngagementData)
+            Log.d("group", "$groupEngagementData")
         }
     }
 
-    private fun testData() {
-        groupEngagementRateItem = arrayListOf(
-            GroupEngagementRateData(
-                1, "testName1", 30
-            ),GroupEngagementRateData(
-                2, "testName2", 12
-            ),GroupEngagementRateData(
-                3, "testName3", 11
-            ),GroupEngagementRateData(
-                4, "testName4", 10
-            ),GroupEngagementRateData(
-                5, "testName5", 9
-            ),GroupEngagementRateData(
-                6,"testName6", 8
-            ),GroupEngagementRateData(
-                7, "testName7", 7
-            ),GroupEngagementRateData(
-                8, "testName8", 6
-            ),GroupEngagementRateData(
-                9, "testName9", 5
-            ),GroupEngagementRateData(
-                10,"testName10", 4
-            ),GroupEngagementRateData(
-                11,"testName11", 3
-            ),GroupEngagementRateData(
-                12,"testName12", 3
-            ),GroupEngagementRateData(
-                13,"testName13", 3
-            ),GroupEngagementRateData(
-                14,"testName14", 2
-            ),GroupEngagementRateData(
-                15,"testName15", 1
-            ),GroupEngagementRateData(
-                16,"testName16", 0
-            )
-        )
+    private fun fetchMembers() {
+        lifecycleScope.launch {
+            try {
+                val members = apiService.getOrganizationMembers("githubapi-testad")
+                for (member in members) {
+                    val repositories = apiService.getOrganizationRepositories("githubapi-testad")
+                    var totalContributions = 0
+                    for (repo in repositories) {
+                        val contributors =
+                            apiService.getRepositoryContributors("githubapi-testad", repo.name)
+                        Log.d("constributors", "$contributors")
+                        val userContributions =
+                            contributors.find { it.login == member.login }?.contributions ?: 0
+                        totalContributions += userContributions
+                    }
+                    Log.d(
+                        "GitHubActivity",
+                        "${member.login} 님의 총 커밋 수: $totalContributions, 프로필 이미지 URL: ${member.avatarUrl}"
+                    )
+                    groupEngagementData.add(
+                        GroupEngagementData(
+                            member.login, member.avatarUrl, totalContributions
+                        )
+                    )
+                }
+                groupViewModel.setEngagementRate(groupEngagementData)
+            } catch (e: Exception) {
+                Log.d("e", "$e")
+            }
+        }
     }
 }
