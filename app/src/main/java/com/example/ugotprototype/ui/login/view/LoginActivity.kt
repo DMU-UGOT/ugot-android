fc0ad0694f61eb26b76aabf98020170ba26f389d
wlsdn9297 <wlsdn9297@naver.com>
2023-08-19 15:01:52 +0900

    카카오 로그인 API 구현

--- app/src/main/java/com/example/ugotprototype/ui/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/ugotprototype/ui/login/view/LoginActivity.kt
@@ -1,89 +1,57 @@
 package com.example.ugotprototype.ui.login.view
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
-import com.example.ugotprototype.MainActivity
 import com.example.ugotprototype.R
 import com.example.ugotprototype.databinding.ActivityLoginBinding
-import com.example.ugotprototype.di.api.ApiService
 import com.example.ugotprototype.ui.login.viewmodel.LoginViewModel
-import com.example.ugotprototype.ui.team.view.TeamFragment
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+    private val kakaoClient: UserApiClient by lazy { UserApiClient.instance }
 
-    @Inject
-    lateinit var apiService: ApiService
-
-    private val loginViewModel: LoginViewModel by viewModels()
+    // 카카오계정으로 로그인 공통 callback 구성
+    // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+    private val kakaOAuthCallback: (OAuthToken?, Throwable?) -> Unit = { token, _ ->
+//        token?.let { viewModel.login(token.accessToken) }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        loginViewModel.setUserName("az1aee") // 닉네임 더미데이터
+        binding.imgKakaoLogin.setOnClickListener { kakaoLogin() }
+    }
 
-        loginViewModel.githubUserName.observe(this) {
-            accountExists(it)
-        }
-
-        binding.btLoginKakao.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-
-        binding.btLoginMainInput.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-
-        binding.btLoginMainFindId.setOnClickListener {
-            openMainEmailUrl()
-            finish()
-        }
-
-        binding.btLoginMainFindPwd.setOnClickListener {
-            openMainPwdUrl()
-            finish()
+    private fun kakaoLogin() {
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (kakaoClient.isKakaoTalkLoginAvailable(this)) {
+            handleKakaoTalkLoginResult()
+        } else {
+            kakaoClient.loginWithKakaoAccount(this, callback = kakaOAuthCallback)
         }
     }
 
-    fun openMainEmailUrl() {
-        val url =
-            "https://accounts.kakao.com/weblogin/find_account?lang=ko&continue=%2Flogin%3Fcontinue%3Dhttps%253A%252F%252Faccounts.kakao.com%252Fweblogin%252Faccount%252Finfo"
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-    }
-
-    fun openMainPwdUrl() {
-        val url =
-            "https://accounts.kakao.com/weblogin/find_password?lang=ko&continue=%2Flogin%3Fcontinue%3Dhttps%253A%252F%252Faccounts.kakao.com%252Fweblogin%252Faccount%252Finfo"
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-    }
-
-    private fun accountExists(githubUserName: String) {
-        lifecycleScope.launch {
-            try {
-                val response =
-                    apiService.getUser(githubUserName, "Bearer ${TeamFragment.tokenData}")
-                Log.d("[계정확인]", "해당 계정 존재 : $response")
-            } catch (e: Exception) {
-                Log.d("[계정확인 불가능]", "해당 계정 미존재")
+    private fun handleKakaoTalkLoginResult() {
+        kakaoClient.loginWithKakaoTalk(this) { token, error ->
+            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+            // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                return@loginWithKakaoTalk
             }
+            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+            error?.let { kakaoClient.loginWithKakaoAccount(this, callback = kakaOAuthCallback) }
+//            token?.let { viewModel.login(token.accessToken) }
         }
     }
 }
